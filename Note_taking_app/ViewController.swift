d24f98a9f35ad60171215061e72dafee3db5e4e9
khushi07 <khushiverma988@gmail.com>
2019-07-23 13:47:36 -0400

    Initial Commit

---
+++ Note_taking_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Note_taking_app
+//
+//  Created by Khushi Verma on 2019-07-23.
+//  Copyright © 2019 Team Stang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
